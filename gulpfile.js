a4b5865e4eafb39eee5a2e1712581a8d5a538f54
Esvethlynna Pangelinan and Lincoln Nguyen <esvethlynna@>
2017-07-31 16:08:59 -0700

    complete rest of exercises for Monday using AJAX and refactoring multiple files

--- gulpfile.js
+++ gulpfile.js
@@ -85,6 +85,7 @@ gulp.task('serve', function() {
 
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
 
 });
 
@@ -93,5 +94,9 @@ gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
 });
 
 gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
   browserSync.reload();
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -24,3 +24,20 @@ $(document).ready(function(){
 $(document).ready(function() {
   $('#time').text(moment());
 });
+
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayHumidity);
+  });
+});
+
+exports.displayHumidityFunction = displayHumidity;
